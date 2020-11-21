29b03c50568f5c6091ce916e7bad72f7e93ee7e3
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-11-21 18:30:03 +0100

    add add_admin_menu modul

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,18 +6,33 @@
  namespace Inc\Pages;
 
  use \Inc\Base\BaseController;
+ use \Inc\API\SettingsApi;
 
  class Admin extends BaseController {
 
+  public $settings;
+
+  public function __construct() {
+    $this->settings = new SettingsApi();
+  }
+
   public function register() {
-    add_action( "admin_menu", array( $this, "add_admin_pages" ) );
-  }
+    // add_action( "admin_menu", array( $this, "add_admin_pages" ) );
 
-  public function add_admin_pages() {
-    add_menu_page( "Braweria Plugin", "Braweria", "manage_options", "braweria_plugin", array( $this, "admin_index"), "dashicons-buddicons-activity", 110 );
-  }
+    $pages = [
+      [
+        "page_title" => "Braweria Plugin",
+        "menu_title" => "Braweria",
+        "capability" => "manage_options",
+        "slug" => "braweria_plugin",
+        "callback" => function() { echo '<h1>Braweria Plugin!</h1>'; },
+        "icon_url" => "dashicons-buddicons-activity",
+        "position" => 110
+      ]
+    ];
 
-  public function admin_index() {
-    require_once $this->plugin_path . "admin/layout.php";
+    
+
+    $this->settings->addPages( $pages )->register();
   }
  }
