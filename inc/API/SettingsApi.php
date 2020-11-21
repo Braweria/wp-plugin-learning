7a46fa955fb620e5645e45bc24b3a1d6f72f8b42
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-11-21 19:13:18 +0100

    subpage modular

--- inc/API/SettingsApi.php
+++ inc/API/SettingsApi.php
@@ -7,6 +7,7 @@
 
  class SettingsApi {
    public $admin_pages = array();
+   public $admin_subpages = array();
 
    public function register() {
      if ( !empty($this->admin_pages) ) {
@@ -20,9 +21,42 @@
     return $this;
   }
 
+  public function withSubPage( string $title = null ) {
+    if ( empty($this->admin_pages) ) {
+      return $this;
+    }
+
+    $admin_page = $this->admin_pages[0];
+
+    $subpage = [
+      [
+        "parent_slug" => $admin_page["slug"],
+        "page_title" => $admin_page["page_title"],
+        "menu_title" => ($title) ? $title : $admin_page['menu_title'],
+        "capability" => $admin_page["capability"],
+        "slug" => $admin_page["slug"],
+        "callback" => $admin_page["callback"]
+      ]
+    ];
+
+    $this->admin_subpages = $subpage;
+    return $this;
+
+  }
+
+  public function addSubPages( array $pages ) {
+    $this->admin_subpages = array_merge( $this->admin_subpages, $pages );
+    return $this;
+  }
+
   public function addAdminMenu() {
     foreach( $this->admin_pages as $page ) {
       add_menu_page( $page["page_title"], $page["menu_title"], $page["capability"], $page["slug"], $page["callback"], $page["icon_url"], $page["position"] );
     }
+
+    foreach( $this->admin_subpages as $page ) {
+      add_submenu_page( $page["parent_slug"], $page["page_title"], $page["menu_title"], $page["capability"], $page["slug"], $page["callback"] );
+    }
+
   }
  }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -10,16 +10,15 @@
 
  class Admin extends BaseController {
 
+  public $pages = array();
+  public $subpages = array();
+
   public $settings;
 
   public function __construct() {
     $this->settings = new SettingsApi();
-  }
 
-  public function register() {
-    // add_action( "admin_menu", array( $this, "add_admin_pages" ) );
-
-    $pages = [
+    $this->pages = [
       [
         "page_title" => "Braweria Plugin",
         "menu_title" => "Braweria",
@@ -31,8 +30,28 @@
       ]
     ];
 
-    
+    $this->subpages = [
+      [
+        "parent_slug" => "braweria_plugin",
+        "page_title" => "Custom post Types",
+        "menu_title" => "CPT",
+        "capability" => "manage_options",
+        "slug" => "braweria_cpt",
+        "callback" => function() { echo '<h1>Custom Post Type Manager</h1>'; }
+      ],
+      [
+        "parent_slug" => "braweria_plugin",
+        "page_title" => "Subpage",
+        "menu_title" => "Sub",
+        "capability" => "manage_options",
+        "slug" => "braweria_sub",
+        "callback" => function() { echo '<h1>Another Subpage</h1>'; }
+      ]
+    ];
+  }
 
-    $this->settings->addPages( $pages )->register();
+  public function register() {
+    // add_action( "admin_menu", array( $this, "add_admin_pages" ) );
+    $this->settings->addPages( $pages )->withSubPage( "Dashboard" )->addSubPages( $this->subpages )->register();
   }
  }
