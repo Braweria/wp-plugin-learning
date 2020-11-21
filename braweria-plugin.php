e00cda77b9f0906ed8144f8b333a49f00b9f30d8
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-11-21 17:01:39 +0100

    adding settings links and de/activation hooks

--- braweria-plugin.php
+++ braweria-plugin.php
@@ -33,17 +33,47 @@ Copyright (C) 2020 Wiktoria Mielcarek
   along with this program.  If not, see https://www.gnu.org/licenses.
 */
 
+/**
+ * Abort if the file is called directly
+ */
 if (!defined("ABSPATH")) {
   die;
 }
-
+/**
+ * Require once the Composer Autoload
+ */
 if ( file_exists( dirname( __FILE__ ) . "/vendor/autoload.php" ) ) {
   require_once dirname( __FILE__ ) . "/vendor/autoload.php";
 }
-
+/**
+ * Define CONSTANTS
+ */
 define( "PLUGIN_PATH", plugin_dir_path(__FILE__) );
 define( "PLUGIN_URL", plugin_dir_url(__FILE__) );
+define( "PLUGIN", plugin_basename(__FILE__) );
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+/**
+ * The code that runs during plugin activation
+ */
+function activate_braweria_plugin() {
+  Activate::activate();
+}
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_braweria_plugin() {
+  Deactivate::deactivate();
+}
+/**
+ * Register Hooks for deactivation and activation
+ */
+register_activation_hook( __FILE__, "activate_braweria_plugin" );
+register_deactivation_hook( __FILE__, "deactivate_braweria_plugin" );
+/**
+ * If class exists run it
+ */
 if ( class_exists( "Inc\\Init" ) ) {
   Inc\Init::register_services();
 }
---
+++ inc/Base/SettingsLinks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package BraweriaPlugin
+ */
+
+namespace Inc\Base;
+
+ class SettingsLinks {
+   public function register() {
+    add_filter( "plugin_action_links_" . PLUGIN, array( $this, "settings_links" ) );
+   }
+
+   public function settings_links( $links ) {
+    $settings_link = '<a href="admin.php?page=braweria_plugin">Settings</a>';
+    array_push( $links, $settings_link );
+    return $links;
+   }
+ }
